8fb6269b39e1995fdb9ef315a4dbdeba81438aea
benb10 <46840126+benb10@users.noreply.github.com>
2024-12-07 20:26:07 +1000

    cleanup

--- backend/app/tests/test_main.py
+++ backend/app/tests/test_main.py
@@ -5,12 +5,7 @@ client = TestClient(app)
 
 
 def test_hello_world():
-    response = client.get('/hello_world')
+    response = client.get("/hello_world")
 
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
-
-    try:
-        1/0
-    except:
-        pass
