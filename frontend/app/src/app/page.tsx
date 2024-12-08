7d422d7ae3d34d0245806ac3cf7cc1eeb95e1b06
benb10 <46840126+benb10@users.noreply.github.com>
2024-12-08 22:12:55 +1000

    prettier

--- frontend/app/src/app/page.tsx
+++ frontend/app/src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       <div className={styles.description}>
         {title}
         backend url: {process.env.NEXT_PUBLIC_API_URL}
-        <br/>
+        <br />
         version: 3
         <p>
           Get started by editing&nbsp;
