647b9138816d03bc3afd5d2751a4c42e4ae2f35d
benb10 <46840126+benb10@users.noreply.github.com>
2024-12-16 07:30:27 +1000

    formatting

--- frontend/app/src/__tests__/testHome.test.tsx
+++ frontend/app/src/__tests__/testHome.test.tsx
@@ -1,30 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import { http, HttpResponse } from 'msw';
-import { setupServer } from 'msw/node';
-import { beforeAll, afterEach, afterAll, vi, expect, test } from 'vitest';
+import { render, screen, waitFor } from "@testing-library/react";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+import React, { useEffect, useState } from "react";
+import { afterAll, afterEach, beforeAll, expect, test, vi } from "vitest";
 
-import Home from "../app/page"
+import Home from "../app/page";
 
-
-export const handlers = [
-  http.get('http://localhost:8000/hello_world', () => {
-    return HttpResponse.json({ message: 'Hello World' })
-  }),
-]
+const handlers = [
+	http.get("http://localhost:8000/hello_world", () => {
+		return HttpResponse.json({ message: "Hello World" });
+	}),
+];
 
 const server = setupServer(...handlers);
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
+test("renders API data without crashing", async () => {
+	render(<Home />);
 
-test('renders API data without crashing', async () => {
-  render(<Home />);
-
-  await waitFor(() => screen.getByText(/Hello World/));
+	await waitFor(() => screen.getByText(/Hello World/));
 });
 
-test('test math', async () => {
-  expect(2+3).toBe(5);
- });
+test("test math", async () => {
+	expect(2 + 3).toBe(5);
+});
